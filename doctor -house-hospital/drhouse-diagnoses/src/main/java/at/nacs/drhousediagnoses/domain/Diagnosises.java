d21bcac545f5beb49bb5a897027a497c09e31cdf
mustafa1mashtah <mashtah.moustafa@gmail.com>
2019-05-02 18:30:55 +0200

    edit html file

--- doctor -house-hospital/drhouse-diagnoses/src/main/java/at/nacs/drhousediagnoses/domain/Diagnosises.java
+++ doctor -house-hospital/drhouse-diagnoses/src/main/java/at/nacs/drhousediagnoses/domain/Diagnosises.java
@@ -3,8 +3,6 @@ package at.nacs.drhousediagnoses.domain;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
